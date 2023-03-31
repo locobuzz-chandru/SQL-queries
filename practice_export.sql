ca3ceaf6dd37533eb0fb4d0b0f4f45d38f3d646c
unknown <chandru151998@gmail.com>
2023-03-31 12:08:50 +0530

    [Chandru]Added. SQL import and export csv file queries

--- practice_export.sql
+++ practice_export.sql
@@ -1,5 +1,39 @@
 select * from person into outfile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/personcsv1.csv'
 fields terminated by ','
-lines terminated by '\n';
+lines terminated by '\r\n';
 
 SHOW VARIABLES LIKE "secure_file_priv";
+
+-- exporting specific columns
+select name, city from person where city='Bangalore' 
+into outfile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/personcsv2.csv'
+fields terminated by ','
+lines terminated by '\r\n';
+
+-- exporting data to a CSV file whose filename contains timestamp
+SET @TS = DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s');
+SET @FOLDER = 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/';
+SET @PREFIX = 'orders';
+SET @EXT    = '.csv';
+SET @CMD = CONCAT("SELECT * FROM orders INTO OUTFILE '",@FOLDER,@PREFIX,@TS,@EXT,
+				   "' FIELDS ENCLOSED BY '\"' TERMINATED BY ';' ESCAPED BY '\"'",
+				   "  LINES TERMINATED BY '\r\n';");
+PREPARE statement FROM @CMD;
+EXECUTE statement;
+
+-- exporting data with column headings
+SELECT 'Order Number', 'Order Date', 'Status'
+UNION ALL
+SELECT * from (SELECT orderNumber, orderDate, status
+    FROM orders order by orderNumber) orders
+    INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/orders_head.csv';
+    
+-- handling null values
+SELECT 
+    orderNumber, orderDate, IFNULL(shippedDate, 'N/A')
+FROM
+    orders INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/orders_null.csv' 
+    FIELDS ENCLOSED BY '"' 
+    TERMINATED BY ';' 
+    ESCAPED BY '"' LINES 
+    TERMINATED BY '\r\n';
--- practice_import.sql
+++ practice_import.sql
@@ -8,3 +8,41 @@ show global variables like 'local_infile';
 SET GLOBAL local_infile=1;
 
 SHOW GRANTS;
+
+-- chnage coulm date format before import
+LOAD DATA LOCAL INFILE 'E:/Projects/SQL-queries/info_csv.csv'
+INTO TABLE information
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(title,@expired_date,amount)
+SET expired_date = STR_TO_DATE(@expired_date, '%m/%d/%Y');
+
+-- change column destination
+Load data Local infile 'E:/Projects/SQL-queries/personcsv.csv'
+into table person
+fields terminated by ','
+lines terminated by '\r\n'
+ignore 1 lines
+(id,@name,age,gender,phone,@city)
+set name = @city, city=@name;
+
+-- ingone any column with @dummy keyword
+Load data Local infile 'E:/Projects/SQL-queries/persondummy_csv.csv'
+into table person
+fields terminated by ','
+lines terminated by '\r\n'
+ignore 1 lines
+(id,name,@dummy,age,gender,phone,city);
+
+SELECT * FROM person order by id;
+truncate table person;
+
+-- select specific columns
+Load data Local infile 'E:/Projects/SQL-queries/oneinfocsv.csv'
+into table oneinfo
+fields terminated by ','
+lines terminated by '\r\n'
+ignore 1 lines
+(@name, @gender,@city)
+set name=@name;
